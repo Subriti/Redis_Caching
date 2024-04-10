075f5411a751759bd6cd5a88ccff97ea40f93d98
Subriti <subritiaryal13@gmail.com>
2024-04-10 12:40:20 +0545

    Add project files.

---
+++ Controllers/MarksheetController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+using System.Text.Json;
+
+namespace Redis_Caching.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MarksheetController : ControllerBase
+    {
+        private readonly ConnectionMultiplexer _redisConnection;
+
+        public MarksheetController(ConnectionMultiplexer redisConnection)
+        {
+            _redisConnection = redisConnection;
+        }
+
+        // Pre-defined array list of marksheets
+        private readonly List<Dictionary<string, object>> predefinedMarksheets = new List<Dictionary<string, object>>
+        {
+            new Dictionary<string, object> { { "user_id", 1 }, { "marks", new Dictionary<string, int> { { "math", 90 }, { "science", 85 }, { "english", 92 } } } },
+            new Dictionary<string, object> { { "user_id", 2 }, { "marks", new Dictionary<string, int> { { "math", 80 }, { "science", 75 }, { "english", 85 } } } },
+            new Dictionary<string, object> { { "user_id", 3 }, { "marks", new Dictionary<string, int> { { "math", 95 }, { "science", 90 }, { "english", 88 } } } }
+        };
+
+        private List<Dictionary<string, object>> GetMarksheets()
+        {
+            var db = _redisConnection.GetDatabase();
+            var marksheets = db.StringGet("marksheets");
+            if (!marksheets.IsNull)
+            {
+                return JsonSerializer.Deserialize<List<Dictionary<string, object>>>(marksheets);
+            }
+            else
+            {
+                db.StringSet("marksheets", JsonSerializer.Serialize(predefinedMarksheets));
+                return predefinedMarksheets;
+            }
+        }
+
+        [HttpGet("{userId}")]
+        public IActionResult GetMarksheet(int userId)
+        {
+            var marksheets = GetMarksheets();
+            foreach (var marksheet in marksheets)
+            {
+                if (int.TryParse(marksheet["user_id"].ToString(), out int userIdFromSheet) && userIdFromSheet == userId)
+                {
+                    return Ok(marksheet);
+                }
+            }
+            return NotFound(new { error = "Student not found" });
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+using StackExchange.Redis;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add Redis caching
+builder.Services.AddSingleton<ConnectionMultiplexer>(sp =>
+{
+    var configuration = ConfigurationOptions.Parse("localhost:6379");
+    return ConnectionMultiplexer.Connect(configuration);
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
